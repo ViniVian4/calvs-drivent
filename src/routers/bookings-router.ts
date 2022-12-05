1a9018ae6266a95ed0d2e1c41aecda2409c1cea5
Vinicius <vianaxd99@gmail.com>
2022-12-05 09:32:21 -0300

    /booking /POST route fix

--- src/routers/bookings-router.ts
+++ src/routers/bookings-router.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { authenticateToken } from "@/middlewares";
-import { getBookings } from "@/controllers";
+import { getBookings, postBooking } from "@/controllers";
 
 const bookingsRouter = Router();
 
 bookingsRouter
   .all("/*", authenticateToken)
   .get("/", getBookings)
-  .post("/");
+  .post("/", postBooking);
 
 export { bookingsRouter };
